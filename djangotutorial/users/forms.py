edaf670c723a41e6100fa0f557b97acfded1c271
Samet Tekin <116036418+samoosama1@users.noreply.github.com>
2026-04-06 21:53:39 +0300

    vcs tracking update

--- djangotutorial/users/forms.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-from .models import User
-
-class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = User
-        fields = ('username', 'email') # You can add other fields from your User model here
--- djangotutorial/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-from . import views
-
-urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path("", include("django.contrib.auth.urls")),
-    path('logout_user/', views.logout_user, name='logout_user'),
-]
